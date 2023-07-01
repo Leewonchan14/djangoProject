9e968b61d09c46cc493f1062eda0a55636b1ae4d
이원찬 <105588857+Leewonchan14@users.noreply.github.com>
2023-07-01 13:33:19 +0900

    로컬 mysql DB 연결

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -75,11 +75,19 @@ WSGI_APPLICATION = "djangoProject.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',               # 데이터베이스 이름
+        'USER': 'django',           # MySQL 사용자 이름
+        'PASSWORD': '0000',           # MySQL 사용자 비밀번호
+        'HOST': 'localhost',                # 데이터베이스 서버 주소 (로컬 서버인 경우)
+        'PORT': '3306',                     # MySQL 포트 (3306이 기본값)
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
+
 
 
 # Password validation
